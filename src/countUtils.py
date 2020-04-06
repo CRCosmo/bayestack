1051166abdbc7b2435d7f6c984019d315f47de27
Eliab Malefahlo <eliabmalefahlo3@gmail.com>
2020-04-06 20:12:20 +0200

    Local changes

--- src/countUtils.py
+++ src/countUtils.py
@@ -15,14 +15,16 @@ from scipy import integrate,stats
 from scipy.interpolate import interp1d
 from scipy.special import erf
 from scipy.stats import rice,rayleigh
+from lumfuncUtils import dNdS_LF,LF, get_dlds, get_Vmax,get_sbins
 import matplotlib.pyplot as plt
+#from bayefit import dataModel,Lopt2Lrad
 from profile_support import profile
 from utils import sqDeg2sr,sqrtTwo,find_nearest,medianArray,\
                            interpol,buildCDF,Jy2muJy,interpola
 
 if 'chains' in sys.argv[-1]:
     potential_settings=glob.glob(os.path.join(sys.argv[-1],'*settings*py'))
-    assert(len(potential_settings)==1), '***More than one potential (or missing) settings file!'
+    #assert(len(potential_settings)==1), '***More than one potential settings file!'
     settingsf='.'.join([sys.argv[-1],potential_settings[0].split('/')[-1].split('.')[-2]])
 else:
     settingsf=sys.argv[-1].split('.')[-2]
@@ -42,8 +44,7 @@ def simulate(family,params,paramsList,bins,\
              seed=None,N=None,noise=None,output=None,\
              dump=None,version=2,verbose=False,area=None,\
              skadsf=None,pole_posns=None,simarrayf=None,\
-             simdocatnoise=True,return_vals=False,\
-             gridlength=None):
+             simdocatnoise=True):
     """
     Based on lumfunc.simtable()
     Specify family + parameters
@@ -104,7 +105,7 @@ def simulate(family,params,paramsList,bins,\
 
     # Initialize seed for variates AND any noise
     if seed is not None:
-        numpy.random.seed(seed=seed)
+        numpy.random.seed(seed=SEED_SIM)
 
     if family=='ppl':
         C=alpha=Smin=Smax=beta=S0=gamma=S1=delta=S2=-99.0
@@ -125,8 +126,7 @@ def simulate(family,params,paramsList,bins,\
         Smax=params[paramsList.index('S%i'%iSmax)]
 
         function = lambda S:powerLawFuncWrap(nlaws,S,C,alpha,-99.0,beta,\
-                                       Smin,Smax,S0,gamma,S1,delta,S2,1.0)
-        #print 'SS',function(Smin)
+                                      Smin/1e6,Smax/1e6,S0/1e6,gamma,S1/1e6,delta,S2/1e6,1.0)
 
     elif family=='test':
         Smin=params[paramsList.index('S0')]
@@ -188,32 +188,121 @@ def simulate(family,params,paramsList,bins,\
             F=numpy.random.choice(F,size=N,replace=False)
         N=len(F)
         print 'NSKADS = %i' % N
+        
+    elif family =='Lrad':
+    	Smin=params[paramsList.index('LoptMIN')]
+    	Smax=params[paramsList.index('LoptMAX')]
+    	A=params[paramsList.index('A')]
+    	B=params[paramsList.index('B')]
+    	sigma_Lrad=params[paramsList.index('sigma_Lrad')]
+    	#print Loptmin,Loptmax
+    	print 'Doing LF simulation'
+    	inta =None
+    	#intg = integrate.quad(lambda Lopt:Lopt2Lrad(Lopt,A=A,B=B,flux=False),Loptmin,Loptmax,epsabs=0.)[0]
+
+    	function = lambda Lopt:Lopt2Lrad(Lopt,A=A,B=B,flux=False)
+    elif family in ['LFsch','LFdpl']:
+        redshift = 0.325
+        z_min = 0.2
+        z_max = 0.45
+    	Lmin=params[paramsList.index('LMIN')]
+    	Lmax=params[paramsList.index('LMAX')]
+    	[Smin,Smax]= SMIN_SIM,SMAX_SIM
+    	print Smin,Smax
+    	[Smin,Smax]= get_sbins([10**Lmin,10**Lmax],redshift,dl)*1e6
+    	print Smin,Smax,Lmin,Lmax
+    	print 'Doing LF simulation'
+    	Vmax=get_Vmax(z_min,z_max)
+    	dsdl = get_dsdl(redshift,dl)
+    	inta =None
+    	intg = integrate.quad(lambda S:LF(S,redshift,dsdl,Vmax,dl,params=params,paramsList=paramsList,\
+             inta=inta,area=area,family=family),Smin*1e-6,Smax*1e-6,epsabs=0.)[0]
+        print intg*Vmax
+        print Vmax
+        area = N/(Vmax*intg)
+        area1=area
+        print N,area
+
+    	function = lambda S:dNdS_LF(S,z_min,redshift,z_max,dl,params=params,paramsList=paramsList,\
+             area=area,family=family)    	 
 
     if family != 'skads':
         # Set up the 'rough' array
-        if gridlength is None:
-            gridlength=1000000 # Good enough to prevent bleeding at edges
+        gridlength=10000 # Good enough to prevent bleeding at the edges
         Ss=numpy.linspace(Smin,Smax,gridlength)
-        values=numpy.array([function(ix) for ix in Ss])
+        print Smin,Smax
+        print 'checking for one sample'
+        kl = function(20/1e6)
+        print kl
+        #sys.exit()
+        values=numpy.array([function(ix/1e6) for ix in Ss])
+        print values[:10]
         # Build the CDF
         CDF=buildCDF(values)
+        plt.plot(CDF,Ss)
+        #plt.xscale('log')
+        #plt.yscale('log')
+        plt.ylabel('Flux')
+        plt.xlabel('CDF')
+        plt.show()
+        print CDF.max()
         # Create the interpolant object
         sampler=interp1d(CDF,Ss)
+        
+        plt.plot(Ss,values,'.')
+        plt.xscale('log')
+        plt.yscale('log')
+        plt.xlabel('Flux')
+        plt.ylabel('LF')
+        plt.show()
+        
+        x = numpy.linspace(0.,1.,10000)
+        z = numpy.logspace(0,1,1000)/10.
+        f = sampler(z)
+        y = sampler(x)
+        plt.yscale('log')
+        #plt.xscale('log')
+        plt.axhline(Smin)
+        plt.axhline(Smax)
+        plt.xlabel('R')
+        plt.ylabel('Sampler(R) [flux]')
+        #plt.plot(x,y)
+        plt.plot(z,f)
+        plt.show()
+        #sys.exit()
 
         # Test that the sampler extrema match
-        print Smin,sampler(0.0)
+        print Smin,sampler(0.0), 'you know wa mean'
         print Smax,sampler(0.99999)
 #        assert(numpy.isclose(sampler(0.0),Smin)[0])
 #        assert(numpy.isclose(sampler(0.99999),Smax,atol=1.0e-3)[0])
 
         # Draw the random deviates
         R = numpy.random.rand(N)
+        print len(R)
         F=sampler(R)
-
+        Nt = 0.
+        for f in F:
+         if f<1.:
+          Nt+=1.
+        F = F[F>1.]
+        print N,Nt
+        print len(F)
+        Nt = len(F)
+        #sys.exit()
+        
         # Normalize here - this is N2C
         # EITHER N is specified explicitly
         # BOTH N2C and C2N are useful
         # Integrate the original function
+        #intg = integrate.quad(lambda S:LF(S,redshift,dsdl,Vmax,dl,params=params,paramsList=paramsList,\
+         #    inta=inta,area=area,family=family),Smin*1e-6,Smax*1e-6,epsabs=0.)[0]
+        #print intg*Vmax
+        #print Vmax
+        #area = Nt/(Vmax*intg)
+        #print N,area,area1
+        #plt.show()
+        #sys.exit()
         A=integrate.quad(function,Smin,Smax)[0]
 #        print A,N
         # Bin the random samples
@@ -243,19 +332,20 @@ def simulate(family,params,paramsList,bins,\
         print 'For %i sources, C is %e (should be %e)' % (N,C_calc,coeffs[0])
 
     # Dump noiseless fluxes to file
-    if dump is not None:
-        puredumpf=dump
-        numpy.savetxt(puredumpf,F)
-        print 'Draws (noiseless) are in %s' % puredumpf
-
+    puredumpf=dump
+    idl_style=False
+    numpy.savetxt(puredumpf,F)
+    print 'Draws (noiseless) are in %s' % puredumpf
+    writeCountsFile(output[1],bins,F,area,idl_style=idl_style,verbose=verbose)
+    print output[1]
     # Now add noise if requested
     if simdocatnoise:
-        numpy.random.seed(seed=seed)
+        numpy.random.seed(seed=SEED_SIM)
         poln=False
         if poln:
             F+=rice.rvs(F/noise,size=N)
         else:
-            F+=numpy.random.normal(0.0,noise,N)
+            F+=numpy.random.normal(0.0,noise,Nt)
 
     # Dump noisy fluxes to file
     if dump is not None:
@@ -263,15 +353,14 @@ def simulate(family,params,paramsList,bins,\
         numpy.savetxt(noisydumpf,F)
         print 'Draws (noisy) are in %s' % noisydumpf
         print 'Minimum flux in catalogue = %f' % F.min()
+        print 'Maximum flux in catalogue = %f' % F.max()
 
     # Write counts file
-    idl_style=False
-    writeCountsFile(output,bins,F,area,idl_style=idl_style,verbose=verbose)
+    print output[0]
+    writeCountsFile(output[0],bins,F,area,idl_style=idl_style,verbose=verbose)
+    print N,area#,area1
 
-    if return_vals:
-        return F,function
-    else:
-        return F
+    return F
 
 #-------------------------------------------------------------------------------
 
@@ -295,9 +384,9 @@ def N2C(function,deviates,Smin,Smax,numbins):
 
 @profile
 def writeCountsFile(output,bins,fluxes,area,idl_style=None,\
-                    version=2,verbose=None,corrs=None):
+                    version=2,verbose=None,corrs=None, redshift=None):
     """
-    Write an array of fluxes in muJy to a binned counts file
+    Write an array of fluxes to a binned counts file
     """
 
     # Test version
@@ -305,44 +394,63 @@ def writeCountsFile(output,bins,fluxes,area,idl_style=None,\
 
     # Bin up the fluxes
     counts=numpy.histogram(fluxes,bins=bins)[0]
+    index = numpy.digitize(fluxes,bins = bins)
+    if redshift is not None:
+       red_mean = numpy.zeros(len(bins))
+       for i in range(len(bins)):
+          red_mean[i] = numpy.mean(redshift[index ==i])
+          #print numpy.mean(redshift[index ==i])         
     N=len(fluxes)
     print '-> %i/%i objects observed in total (after binning)\n' % (counts.sum(),N)
-
+    
     # Calculate differential counts
     idl_style=False
-    dn_by_ds,dn_by_ds_eucl,dn_by_ds_errs,dn_by_ds_b,dn_by_ds_b_errs=\
-      calculateDnByDs(1.0e-6*bins,counts,idl_style=idl_style,return_all=True)
+    print bins
+    
+    #dn_by_ds,dn_by_ds_eucl,dn_by_ds_errs,dn_by_ds_b,dn_by_ds_b_errs=\
+    #  calculateDnByDs(1.0e-6*bins,counts,idl_style=idl_style,return_all=True)
+    #dn_by_ds,dn_by_ds_eucl,dn_by_ds_errs,dn_by_ds_b,dn_by_ds_b_errs=\
+    #  calculateDnByDs(1.0e-6*bins,counts,idl_style=idl_style,return_all=True,verbose=1)
 
     median_bins=medianArray(bins) # uJy
     NB=len(median_bins)
 
     # Set up the corrections
     if corrs is None:
-        corrs=numpy.ones(NB)
+        corrs=numpy.ones(NB)	
 
     if output is not None:
         outputf=output
         s=open(outputf,'w')
         if version < 2:
             header='# bin_median ksRaw ksNoisy'
+        elif redshift is not None:
+              header='# bin_low_uJy bin_high_uJy bin_median_uJy Ns_tot_obs Redshift'
+
         else:
             header='# bin_low_uJy bin_high_uJy bin_median_uJy Ns_tot_obs dnds_srm1Jym1 dnds_eucl_srm1Jy1p5 delta_dnds_eucl_lower_srm1Jy1p5 delta_dnds_eucl_upper_srm1Jy1p5 corr Ncgts_degm2 dNcgts_lower_degm2 dNcgts_upper_degm2'
         s.write('%s\n'%header)
         if verbose: print header
-        for ibin in range(NB):
+        for ibin in range(NB -1):
             if version < 2:
                 line='%f %i %i' % (median_bins[ibin],-99.0,counts[ibin])
+                
+            elif redshift is not None:
+                    line='%f %f %f %i %f' % (bins[ibin],bins[ibin+1],median_bins[ibin],counts[ibin],red_mean[ibin+1])
+                   
+           
             else:
+                     
                 line='%f %f %f %i %e %e %e %e %f %i %i %i' % \
-                  (bins[ibin],bins[ibin+1],median_bins[ibin],round(corrs[ibin]*counts[ibin]),\
+                  (bins[ibin],bins[ibin+1],median_bins[ibin],round(counts[ibin]),\
                    dn_by_ds[ibin]/(sqDeg2sr*area),\
                    dn_by_ds_eucl[ibin]/(sqDeg2sr*area),\
                    dn_by_ds_errs[ibin]/(sqDeg2sr*area),\
                    dn_by_ds_errs[ibin]/(sqDeg2sr*area),\
                    corrs[ibin],\
-                   round(counts[ibin:].sum()*1.00), #/area),\
-                   round(numpy.sqrt(counts[ibin:].sum()*1.00)), #/area)),\
-                   round(numpy.sqrt(counts[ibin:].sum()*1.00))) #/area)))
+                   round(counts[ibin:].sum()*1.00/area),\
+                   round(numpy.sqrt(counts[ibin:].sum()*1.00/area)),\
+                   round(numpy.sqrt(counts[ibin:].sum()*1.00/area)))
             s.write('%s\n'%line)
             if verbose: print line
         print counts.sum()
@@ -350,12 +458,12 @@ def writeCountsFile(output,bins,fluxes,area,idl_style=None,\
 
         print 'Look in %s' % outputf
 
-    return counts
+    return
 
 #-------------------------------------------------------------------------------
 
 @profile
-def calculateDnByDs(bins,counts,eucl=False,verbose=False,idl_style=True,
+def calculateDnByDs(bins,counts,eucl=True,verbose=False,idl_style=False,
                     errors=False,bright=False,bright_errors=False,
                     return_all=False):
     """
@@ -385,10 +493,21 @@ def calculateDnByDs(bins,counts,eucl=False,verbose=False,idl_style=True,
         print 'ds',ds
 
     if return_all:
+        if verbose:
+         print 'dnds',dn/ds
+         print 'dnds eucl', (Smed**2.5) * dn/ds
+         print 'dnds bright', (Smed**2.0) * dn/ds
+         #return dn/ds, (Smed**2.5) * dn/ds,\
+          #(Smed**2.5) * numpy.sqrt(dn)/ds,\
+          #(Smed**2.0) * dn/ds,\
+          #(Smed**2.0) * numpy.sqrt(dn)/ds
+          
         return dn/ds, (Smed**2.5) * dn/ds,\
           (Smed**2.5) * numpy.sqrt(dn)/ds,\
           (Smed**2.0) * dn/ds,\
-          (Smed**2.0) * numpy.sqrt(dn)/ds
+          (Smed**2.0) * numpy.sqrt(dn)/ds,\
+          Smed
+
 
     if eucl:
         dn_by_ds = (Smed**2.5) * dn/ds
@@ -575,15 +694,11 @@ def polyFunc(S,S_1,Smin,Smax,c):
         return 0.0
     #assert(len(c)==3)
     exponent=0.0
-    S_1*=1.0e-6
     for n in range(len(c)):
-        exponent += c[n] * (numpy.log10(S/S_1))**n
-        #exponent += c[n]*(S**n)
+        #exponent += c[n] * (numpy.log10(S/S_1)**n)
+        exponent += c[n]*(S**n)
     #print c
-    #print exponent
-    func=numpy.poly1d(list(reversed(c)))
-    return func(S/S_1)
-    #return exponent
+    return exponent
     #return 10**exponent
 
 #-------------------------------------------------------------------------------
@@ -628,7 +743,7 @@ def polesFunc(S,pole_posns,Smin,Smax,coeffs):
 
 @profile
 def calculateI(params,paramsList,bins=None,area=None,
-                family=None,dump=None,verbose=False,model=None,noiseZone=-1):
+                family=None,dump=None,verbose=False,model=None):
 
     """
     pn_integral, but for various different function families
@@ -637,7 +752,7 @@ def calculateI(params,paramsList,bins=None,area=None,
 
     if family=='ppl':
         return calculateI3(params,paramsList,bins=bins,area=area,\
-                family=family,dump=dump,verbose=verbose,noiseZone=noiseZone)
+                family=family,dump=dump,verbose=verbose)
 
     elif family=='poly':
         Smin=params[paramsList.index('S0')]
@@ -645,10 +760,8 @@ def calculateI(params,paramsList,bins=None,area=None,
         coeffs=[params[paramsList.index(p)] for p in paramsList if p.startswith('p')]
 #        func=numpy.poly1d(list(reversed(coeffs)))
 #        ncoeffs=len(coeffs)
-        S_1=1.0 # ref flux / Jy
-        #noise=params[paramsList.index('noise')]
-        if noiseZone==-1:noiseZone=0
-        noise=params[paramsList.index('noise%i'%noiseZone)]
+        S_1=1.0 # ref flux
+        noise=params[paramsList.index('noise')]
 
         #for i in range(6): print params[i]
         nbins=len(bins)-1
@@ -676,7 +789,7 @@ def calculateI(params,paramsList,bins=None,area=None,
 
 @profile
 def calculateI3(params,paramsList,bins=None,area=None,\
-                family=None,dump=None,verbose=False,noiseZone=-1):
+                family=None,dump=None,verbose=False):
 
     """
     Do this for all bins simultaneously
@@ -699,11 +812,8 @@ def calculateI3(params,paramsList,bins=None,area=None,\
 
     C=alpha=Smin=Smax=beta=S0=gamma=S1=delta=S2=-99.0
 
-    #nlaws=int(0.5*len(paramsList)-1)+2-numNoiseZones
-    nlaws=int(0.5*(len(paramsList)-numNoiseZones-1))
+    nlaws=int(0.5*len(paramsList)-1)
 
-    if noiseZone==-1:noiseZone=''
-    
     C=params[paramsList.index('C')]
     Smin=params[paramsList.index('S0')]
     alpha=params[paramsList.index('a0')]
@@ -720,7 +830,7 @@ def calculateI3(params,paramsList,bins=None,area=None,\
     iSmax=int([i for i in paramsList if i.startswith('S')][-1][-1])
     Smax=params[paramsList.index('S%i'%iSmax)]
 
-    noise=params[paramsList.index('noise%i'%noiseZone)]
+    noise=params[paramsList.index('noise')]
 
     nbins=len(bins)
     II = numpy.zeros(nbins-1)
